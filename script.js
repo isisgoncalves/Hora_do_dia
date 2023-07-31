71c2e1ef6bc95fe31cadda0f1be44f2f863154f7
isisgoncalves <isisvow@gmail.com>
2023-07-31 18:32:49 -0300

    Fim do projeto
    Finalizando interação da página com javascript

--- script.js
+++ script.js
@@ -3,14 +3,17 @@ function carregar(){
   let img = document.getElementById('imagem')
   let data = new Date()
   //let hora = data.getHours()
-  let hora = 19
+  let hora = 10
   msg.innerHTML = `Agora são ${hora} horas.`
   if (hora >= 6 && hora < 12){
     img.src = 'images/manha.png'
+    document.body.style.background = '#3c9eea'
   } else if(hora >= 12 && hora < 18){
     img.src ='images/tarde.png'
+    document.body.style.background = '#bc5f00'
   } else{
     img.src = 'images/noite.png'
+    document.body.style.background = '#010e28'
   }
   
 }
